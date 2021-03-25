33507051416df00867100b21db48606f1dd1fae2
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-03-26 00:42:40 +0900

    bug fix on tests

--- test/test.ts
+++ test/test.ts
@@ -46,7 +46,7 @@ describe('Tests for japaneseNumeral.', () => {
     assert.deepEqual([ '八百六十三' ], findKanjiNumbers('今日のランチは八百六十三円でした。'))
     assert.deepEqual([ '八六三' ], findKanjiNumbers('今日のランチは八六三円でした。'))
     assert.deepEqual([ '三千' ], findKanjiNumbers('今月のお小遣いは三千円です。'))
-    assert.deepEqual([ '五', '６２', '８' ], findKanjiNumbers('青森県五所川原市金木町喜良市千苅６２−８'))
+    assert.deepEqual([ '五', '千', '６２', '８' ], findKanjiNumbers('青森県五所川原市金木町喜良市千苅６２−８'))
     assert.deepEqual([ '1億2000万' ], findKanjiNumbers('わたしは1億2000万円もっています。'))
   })
 
